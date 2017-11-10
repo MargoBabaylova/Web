ee81cc1e5c75a7500a6765183a11bb85c66377d7
MargoBabaylova <31851183+MargoBabaylova@users.noreply.github.com>
2017-11-11 03:01:08 +0500

    Добавила ЛР 2

---
+++ LR 2/LR 2/Models/MessageModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LR_2.Models
+{
+    public class MessageModel
+    {
+        [Required(ErrorMessage = "Необходимо ввести адрес")]
+        [EmailAddress(ErrorMessage = "Неверный формат email")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Необходимо ввести тему")]
+        [StringLength(50, ErrorMessage = "Тема не должна превышать 50 символов")]
+        public string Topic { get; set; }
+
+        [Required(ErrorMessage = "Необходимо ввести сообщение")]
+        [StringLength(2000, ErrorMessage = "Сообщение слишком длинное.")]
+        public string Text { get; set; }
+    }
+}
